5c7bf6ab13f68b1da3b581faf12e322bd244dbdd
Min Zhou <minzhou@bu.edu>
2018-11-11 07:08:07 -0500

    update app.py

--- app/app.py
+++ app/app.py
@@ -9,6 +9,10 @@ __copyright__   = "Copyright 2018, minzhou@bu.edu"
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+from subprocess import call, Popen, PIPE
+from shutil import copyfile
+import cv2
+
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -16,21 +20,29 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app = Flask(__name__, static_url_path='/static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# @app.route('/predict_api', methods=['POST'])
-# def predict():
-#      # Error checking
-#      data = request.get_json(force=True)
+# predict and parse the output of yolo model
+parent_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+darknet_path = os.path.join(parent_path, 'yolo_model', 'darknet')
+uploads_path = os.path.join(os.getcwd(), 'uploads')
 
-#      # Convert JSON to numpy array
-#      predict_request = [data['sl'],data['sw'],data['pl'],data['pw']]
-#      predict_request = np.array(predict_request)
+def predict(input_image_path, threh=0.001):
+    current_path = os.getcwd()
+    os.chdir(darknet_path)
+    p = Popen(['./darknet', 'detector', 'test', 
+        '../cfg/rsna.data', '../cfg/rsna_yolov3.cfg_test', 
+        '../backup/rsna_yolov3_900.weights', input_image_path, 
+        '-thresh', f'{threh}'], stdout=PIPE)
+    output = p.communicate()[0]
+    os.chdir(current_path)
+    return output
 
-#      # Predict using the random forest model
-#      y = random_forest_model.predict(predict_request)
-
-#      # Return prediction
-#      output = [y[0]]
-#      return jsonify(results=output)
+def parse_prediction_result(output):
+    output = output[output.find('seconds.')+len('seconds.')+1:].replace('\n', '-').split('-')
+    print(len(output))
+    try:
+        return(output[:5])
+    except:
+        return output
 
 # Home
 @app.route('/')
@@ -62,8 +74,17 @@ def upload():
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                input_image_path = os.path.abspath(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # predict using yolo model
+                output = predict(input_image_path, 0.001)
+                # output = parse_prediction_result(output)
+                # copy the prediciton result to static folder
+                prediction_path = os.path.join(darknet_path, 'predictions.jpg')
+                oriimage = cv2.imread(prediction_path)
+                newimage = cv2.resize(oriimage,(512,512))
+                cv2.imwrite('static/predictions.jpg', newimage)                
                 invalidImage = 2
-                return render_template('upload.html', invalidImage=invalidImage)
+                return render_template('upload.html', invalidImage=invalidImage, filename=filename, output=output)
             else:
                 invalidImage = 1
                 return render_template('upload.html', invalidImage=invalidImage)
@@ -74,7 +95,6 @@ def upload():
     except:
         invalidImage = 3
         return render_template('upload.html', invalidImage=invalidImage)
-
 
 
 # About
